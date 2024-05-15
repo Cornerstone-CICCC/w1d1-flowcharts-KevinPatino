5c0b9736e0f8ad0a34fe20ad3958aa55073c9d02
Kevin Patino <kevinpatcastrejon95@gmail.com>
2024-05-14 21:45:01 -0700

    Excercise 3

---
+++ js/Excercise3.js
@@ -0,0 +1,9 @@
+let A = 10, B = 8, C = 13, text = 'the larges number is: '
+if (A >= B && A >= C) {
+    console.log(text, A);
+} else if (B >= C) {
+    console.log(text, B);
+}
+else {
+ console.log(text, C);
+}

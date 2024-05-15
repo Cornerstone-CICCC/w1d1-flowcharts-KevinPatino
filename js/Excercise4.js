a850aa3728f0c14eaf59354666d8210577534bd5
Kevin Patino <kevinpatcastrejon95@gmail.com>
2024-05-14 21:49:13 -0700

    Excercise 4

---
+++ js/Excercise4.js
@@ -0,0 +1,7 @@
+let A = 2, B = 2, C = 4, text = 'The result is: '
+let AB = A + B
+if (AB > C) {
+    console.log(text, 'Triangle is posible :)')
+} else {
+    console.log(text, 'Triangle is not posible :(')
+}
